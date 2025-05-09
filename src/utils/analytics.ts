0bd43468857b99a2ea726242a21f45fa9545ac81
sumitgundawar <sumitgundawar3@gmail.com>
2025-05-09 14:14:46 +0100

    Updated Google Analytics functionality

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -21,6 +21,7 @@ export const trackEvent = ({ action, category, label }: {
   category: string;
   label?: string;
 }) => {
+  console.log('[GA Event]', { action, category, label });
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
